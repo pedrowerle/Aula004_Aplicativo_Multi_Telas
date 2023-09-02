8a30439f7f59d41ec023e56719cf055857f00df1
Pedro Werle <pedrowerle3@gmail.com>
2023-09-02 11:05:36 -0300

    Initial commit

---
+++ app/src/main/java/com/example/aula004_aplicativo_multi_telas/LancamentoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.aula004_aplicativo_multi_telas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LancamentoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lancamento)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Aula004_Aplicativo_Multi_Telas"
+include(":app")
+ 
